6a9b825fd4dc0bdb4f5ba204d2b2ba991b854baf
František Dvořák <valtri@civ.zcu.cz>
2015-06-30 15:48:46 +0200

    Separated out parsing the body.

--- CORE.py
+++ CORE.py
@@ -8,8 +8,34 @@ import my_config
 
 results = []
 
-def DISCOVERY001():
+category = []
+
+
+def get_categories():
     body, h, http_status, content_type = occi_curl()
+
+    for line in body:
+        item = {}
+        chunks = re.split(';', line)
+        if re.match(r'^Category:', chunks[0]):
+            value = re.sub(r'^Category:\s+', '', chunks[0])
+            item['id'] = value
+
+            # skip the first chunk (category)
+            for chunk in chunks[1:]:
+                keyvalue = re.split(r'\s*=\s*', chunk, 1)
+                print keyvalue
+                key = keyvalue[0]
+                value = keyvalue[1].strip('"')
+                item[key] = value
+
+            category.append(item)
+
+    return [body,  h, http_status, content_type]
+
+
+def DISCOVERY001():
+    body, h, http_status, content_type = get_categories()
 
     err_msg = []
     check01 = False
