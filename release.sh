a8eed4c210e30d56c3e01ae81fbfa0736f06e046
František Dvořák <valtri@civ.zcu.cz>
2017-03-13 15:14:58 +0100

    Release 1.0.3

--- release.sh
+++ release.sh
@@ -2,9 +2,12 @@
 version='1.0.3'
 
 echo "__version__ = '${version}'" > pOCCI/version.py
+vim LICENSE
+git commit -m "Release ${version}"
 python setup.py sdist
 python setup.py test
 twine upload dist/pOCCI-${version}.tar.gz
+git tag -a v${version} -m "Release ${version}."
 git push --tags origin HEAD
 
 # documentation
